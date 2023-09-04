f7f123c7745c32e6d63debb5de9c4e7408782bc2
Muhammad Raihan Wahyu Yuwono <raihanyuwono@hotmail.com>
2023-09-04 15:31:10 +0700

    storage

--- projects/client/src/storage/index.js
+++ projects/client/src/storage/index.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-const reducer = {};
+const reducer = {
+  // Add Reducer Here 
+  // ex -> report: ReportReducer
+};
 
 const Storage = configureStore({ reducer });
 
