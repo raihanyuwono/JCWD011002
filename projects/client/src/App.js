f07c1dc85d2108d72e7b059863e557a95acdbcb7
Febry Dharmawan Junior <83161819+febrydjr@users.noreply.github.com>
2023-09-04 18:50:58 +0700

    Mwec 18 (#6)
    * select address
    
    * bg color
    
    * edit, add, select address button
    
    * add dummy
    
    * input color
    
    * shipping to?

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,9 +1,10 @@
 import Login from "./components/Login";
-
+import SelectAddress from "./components/order/SelectAddress";
 function App() {
   return (
     <>
       <Login />
+      <SelectAddress />
     </>
   );
 }
