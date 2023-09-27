8bc7f604a27d116301f761ca1414b907e0cdf62d
raihanyuwono <raihanyuwono@hotmail.com>
2023-09-27 17:09:06 +0700

    delete dummy

--- projects/client/src/components/Homepage/ProductList.jsx
+++ projects/client/src/components/Homepage/ProductList.jsx
@@ -6,35 +6,6 @@ import { useSelector } from "react-redux";
 import Pagination from "../Utility/Pagination";
 import { useSearchParams } from "react-router-dom";
 
-const dummy = [
-  {
-    id: 1,
-    name: "Zifriend ZA68 - Hyacinth",
-    price: 399000,
-    description: "ZIFRIEND ZA68 RGB Hotswap Mechanical Keyboard",
-    image:
-      "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2023/5/26/c4ace32b-dcbc-4a29-8ca2-44e11fb79a0c.jpg.webp?ect=4g",
-    is_active: true,
-    category: {
-      name: "Keyboard",
-    },
-    stock: 30,
-  },
-  {
-    id: 2,
-    name: "Zifriend ZA68 - Time Machine",
-    price: 389000,
-    description: "ZIFRIEND ZA68 RGB Hotswap Mechanical Keyboard",
-    image:
-      "https://lzd-img-global.slatic.net/g/p/3631c17829cf3ad43bc3bb852a85b062.jpg_720x720q80.jpg_.webp",
-    is_active: true,
-    category: {
-      name: "Keyboard",
-    },
-    stock: 50,
-  },
-];
-
 const container = {
   templateColumns: "repeat(auto-fill, minmax(240px, 1fr))",
   px: ["16px", "8px", "16px", "32px"],
