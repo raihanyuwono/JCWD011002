18a7d6daecd464fdc92fb89de3ffc431bef1fcb8
Febry Dharmawan Junior <febrydj99@gmail.com>
2023-10-18 23:23:44 +0700

    bugs

--- projects/client/src/components/Footer/Footer.jsx
+++ projects/client/src/components/Footer/Footer.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { Box, Flex, Link, Text, VStack, HStack, Icon, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Link,
+  Text,
+  VStack,
+  HStack,
+  Icon,
+  Image,
+} from "@chakra-ui/react";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <Box bg="primary" p={4} w={"full"}>
-      <Flex justify="space-between" align="center" direction={["column", "row"]}>
+    <Box bg="primary" zIndex={999} p={4} w={"full"}>
+      <Flex
+        justify="space-between"
+        align="center"
+        direction={["column", "row"]}
+      >
         <VStack spacing={2} textAlign={["center", "left"]}>
           <Text fontSize="lg" fontWeight="bold">
             Sitemap
